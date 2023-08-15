26c75a24cde455f7ca4f074f5ecee901e16218f8
lexman02 <10473497+lexman02@users.noreply.github.com>
2023-08-14 22:42:06 -0400

    feat: implement backend and frontend config
    Resolves #15
    
    Signed-off-by: lexman02 <10473497+lexman02@users.noreply.github.com>

--- frontend/src/pages/Settings.svelte
+++ frontend/src/pages/Settings.svelte
@@ -1,6 +1,8 @@
 <script>
     import Header from "../components/Header.svelte";
     import Tabs from "../components/Tabs.svelte";
+    import {parseJSON} from "../utils.ts";
+    import config from "../../../config.json";
 
     const importTabs = [
         {
@@ -28,11 +30,21 @@
     ];
 
     let activeImportTab;
-    // TODO: set defaut from config
-    let activeTicketingTab;
+    let activeTicketingTab = setActiveTicketingTab();
     let disabled;
     let input;
     let confirmed = false;
+
+    function setActiveTicketingTab() {
+        switch (config.ticketing.Platform) {
+            case "osTicket":
+                return 2;
+            case "Zammad":
+                return 3;
+            default:
+                return 1;
+        }
+    }
 
     async function submitImport() {
         await fetch("http://localhost:3000/api/config", {
@@ -53,18 +65,6 @@
 
     function exportConfig() {
         this.setAttribute("href", "http://localhost:3000/api/config");
-    }
-
-    function parseJSON(value) {
-        try {
-            let o = JSON.parse(value);
-            if (o && typeof o === 'object') {
-                return o;
-            }
-        } catch (e) {
-            console.log(e);
-            return false;
-        }
     }
 
     const validateFields = (event) => {
@@ -98,12 +98,12 @@
             to Keycloak</a>
     </div>
     <!--{{&#45;&#45;        <hr class="border-2 rounded-2xl border-gray-600/30">&#45;&#45;}}-->
-<!--    @if(Auth::hasRole(config('sync.admin_role'), 'master-realm'))-->
+    <!--    @if(Auth::hasRole(config('sync.admin_role'), 'master-realm'))-->
     <Header title="Admin Settings"/>
 
     <div class="space-y-3 sm:space-y-6">
         <div class="flex flex-col bg-gray-900/50 rounded-xl px-6 py-4 space-y-3 sm:space-y-4">
-<!--             x-data="{ confirmed: @entangle('confirmed') }">-->
+            <!--             x-data="{ confirmed: @entangle('confirmed') }">-->
             <div class="flex flex-col sm:flex-row space-y-2 sm:space-y-0 justify-between items-center">
                 <div>
                     <h5 class="text-xl text-white text-center sm:text-left">Backup & Restore</h5>
@@ -122,8 +122,8 @@
                 </div>
             </div>
             {#if confirmed}
-            <div class="self-center p-2 max-w-fit bg-red-900 items-center mb-4 text-indigo-100 leading-none rounded-full flex lg:inline-flex"
-                 role="alert">
+                <div class="self-center p-2 max-w-fit bg-red-900 items-center mb-4 text-indigo-100 leading-none rounded-full flex lg:inline-flex"
+                     role="alert">
                 <span class="flex rounded-full bg-red-600/60 uppercase px-1 sm:px-2 py-1 text-xs text-white/80 font-bold mr-2">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
                          class="w-5 h-5">
@@ -132,73 +132,73 @@
                               clip-rule="evenodd"/>
                     </svg>
                 </span>
-                <span class="font-semibold mr-2 text-white/80 text-left flex-auto">Are you sure? This will overwrite your current app settings and can't be undone!</span>
-            </div>
-            <Tabs items={importTabs} bind:activeTabValue={activeImportTab}>
-                {#if activeImportTab === 1}
-                    <div class="flex justify-center">
-                        <form on:submit|preventDefault={submitImport}
-                              class="flex flex-col justify-center space-y-2 sm:space-y-4 items-center sm:w-1/2 py-5 bg-gray-800/70 rounded-xl"
-                              id="file">
-    <!--                        <label for="" class="text-white/70 font-semibold text-xl">Upload a valid JSON config</label>-->
-                            <div
-                                    class="flex flex-col w-11/12 sm:w-4/6 max-w-full sm:p-5 p-2 bg-gray-700/30 rounded-xl space-y-3">
-    <!--                            @error('config')-->
-    <!--                            <div-->
-    <!--                                    class="self-center p-2 max-w-fit bg-red-900 items-center text-indigo-100 leading-none rounded-full flex lg:inline-flex"-->
-    <!--                                    role="alert">-->
-    <!--                                        <span-->
-    <!--                                                class="flex rounded-full bg-red-600/60 uppercase px-1 sm:px-2 py-1 text-xs text-white/80 font-bold mr-3">-->
-    <!--                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"-->
-    <!--                                                 class="w-5 h-5">-->
-    <!--                                                <path fill-rule="evenodd"-->
-    <!--                                                      d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-5a.75.75 0 01.75.75v4.5a.75.75 0 01-1.5 0v-4.5A.75.75 0 0110 5zm0 10a1 1 0 100-2 1 1 0 000 2z"-->
-    <!--                                                      clip-rule="evenodd"/>-->
-    <!--                                            </svg>-->
-    <!--                                        </span>-->
-    <!--                                <span class="font-semibold mr-1 text-white/80 text-left flex-auto">{{ $message }}</span>-->
-    <!--                            </div>-->
-    <!--                            @enderror-->
-                                <input type="file"
-                                       accept=".json"
-                                       on:invalid={() => disabled = true}
-                                       on:input={validateFields}
-                                       bind:value={input}
-                                       class="text-white/70 focus:outline-0 file:bg-blue-900/60 file:mr-2.5 file:font-['Nunito'] file:font-semibold file:text-white file:py-2 file:px-4 file:rounded-xl file:border-0">
-                            </div>
-                            <button type="submit"
-                                    {disabled}
-                                    class="w-1/3 p-3 rounded-xl bg-blue-900/50 text-white hover:smooth-hover hover:bg-blue-900/70 focus:outline-none focus:ring-2 disabled:bg-blue-900/30 disabled:text-white/50 disabled:cursor-not-allowed">
-                                Save
-                            </button>
-                        </form>
-                    </div>
-                {:else if activeImportTab === 2}
-                    <div class="flex justify-center h-60">
-                        <form on:submit|preventDefault={submitImport}
-                              class="flex flex-col justify-center items-center sm:w-3/4 sm:h-full p-5 bg-gray-800/70 rounded-xl"
-                              id="input">
-                            <label for="input-textbox"
-                                   class="text-white/70 self-start sr-only">Paste your config here</label>
-                            <textarea data-gramm="false"
-                                      autocomplete="off"
-                                      autocorrect="off"
-                                      autocapitalize="off"
-                                      spellcheck="false"
-                                      id="input-textbox"
-                                      bind:value={input}
-                                      on:input={validateFields}
-                                      placeholder="Paste config here"
-                                      class="bg-gray-700/50 rounded-xl text-sm h-full w-full text-white/75 caret-white p-4 resize-none border-0 ring-1 ring-slate-900/10 focus:outline-none placeholder:font-medium placeholder:text-5xl placeholder:text-center"></textarea>
-                            <button type="submit"
-                                    {disabled}
-                                    class="w-1/3 p-3 rounded-xl bg-blue-900/50 mt-4 text-white hover:smooth-hover hover:bg-blue-900/70 focus:outline-none focus:ring-2 disabled:bg-blue-900/30 disabled:text-white/50 disabled:cursor-not-allowed">
-                                Save
-                            </button>
-                        </form>
-                    </div>
-                {/if}
-            </Tabs>
+                    <span class="font-semibold mr-2 text-white/80 text-left flex-auto">Are you sure? This will overwrite your current app settings and can't be undone!</span>
+                </div>
+                <Tabs items={importTabs} bind:activeTabValue={activeImportTab}>
+                    {#if activeImportTab === 1}
+                        <div class="flex justify-center">
+                            <form on:submit|preventDefault={submitImport}
+                                  class="flex flex-col justify-center space-y-2 sm:space-y-4 items-center sm:w-1/2 py-5 bg-gray-800/70 rounded-xl"
+                                  id="file">
+                                <!--                        <label for="" class="text-white/70 font-semibold text-xl">Upload a valid JSON config</label>-->
+                                <div
+                                        class="flex flex-col w-11/12 sm:w-4/6 max-w-full sm:p-5 p-2 bg-gray-700/30 rounded-xl space-y-3">
+                                    <!--                            @error('config')-->
+                                    <!--                            <div-->
+                                    <!--                                    class="self-center p-2 max-w-fit bg-red-900 items-center text-indigo-100 leading-none rounded-full flex lg:inline-flex"-->
+                                    <!--                                    role="alert">-->
+                                    <!--                                        <span-->
+                                    <!--                                                class="flex rounded-full bg-red-600/60 uppercase px-1 sm:px-2 py-1 text-xs text-white/80 font-bold mr-3">-->
+                                    <!--                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"-->
+                                    <!--                                                 class="w-5 h-5">-->
+                                    <!--                                                <path fill-rule="evenodd"-->
+                                    <!--                                                      d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-5a.75.75 0 01.75.75v4.5a.75.75 0 01-1.5 0v-4.5A.75.75 0 0110 5zm0 10a1 1 0 100-2 1 1 0 000 2z"-->
+                                    <!--                                                      clip-rule="evenodd"/>-->
+                                    <!--                                            </svg>-->
+                                    <!--                                        </span>-->
+                                    <!--                                <span class="font-semibold mr-1 text-white/80 text-left flex-auto">{{ $message }}</span>-->
+                                    <!--                            </div>-->
+                                    <!--                            @enderror-->
+                                    <input type="file"
+                                           accept=".json"
+                                           on:invalid={() => disabled = true}
+                                           on:input={validateFields}
+                                           bind:value={input}
+                                           class="text-white/70 focus:outline-0 file:bg-blue-900/60 file:mr-2.5 file:font-['Nunito'] file:font-semibold file:text-white file:py-2 file:px-4 file:rounded-xl file:border-0">
+                                </div>
+                                <button type="submit"
+                                        {disabled}
+                                        class="w-1/3 p-3 rounded-xl bg-blue-900/50 text-white hover:smooth-hover hover:bg-blue-900/70 focus:outline-none focus:ring-2 disabled:bg-blue-900/30 disabled:text-white/50 disabled:cursor-not-allowed">
+                                    Save
+                                </button>
+                            </form>
+                        </div>
+                    {:else if activeImportTab === 2}
+                        <div class="flex justify-center h-60">
+                            <form on:submit|preventDefault={submitImport}
+                                  class="flex flex-col justify-center items-center sm:w-3/4 sm:h-full p-5 bg-gray-800/70 rounded-xl"
+                                  id="input">
+                                <label for="input-textbox"
+                                       class="text-white/70 self-start sr-only">Paste your config here</label>
+                                <textarea data-gramm="false"
+                                          autocomplete="off"
+                                          autocorrect="off"
+                                          autocapitalize="off"
+                                          spellcheck="false"
+                                          id="input-textbox"
+                                          bind:value={input}
+                                          on:input={validateFields}
+                                          placeholder="Paste config here"
+                                          class="bg-gray-700/50 rounded-xl text-sm h-full w-full text-white/75 caret-white p-4 resize-none border-0 ring-1 ring-slate-900/10 focus:outline-none placeholder:font-medium placeholder:text-5xl placeholder:text-center"></textarea>
+                                <button type="submit"
+                                        {disabled}
+                                        class="w-1/3 p-3 rounded-xl bg-blue-900/50 mt-4 text-white hover:smooth-hover hover:bg-blue-900/70 focus:outline-none focus:ring-2 disabled:bg-blue-900/30 disabled:text-white/50 disabled:cursor-not-allowed">
+                                    Save
+                                </button>
+                            </form>
+                        </div>
+                    {/if}
+                </Tabs>
             {/if}
         </div>
 
@@ -223,11 +223,9 @@
                         <div class="text-white/60 font-medium">
                             <label for="description">Description placeholder message</label>
                             <!--                        @error('description') <span class="text-red-700 text-sm">{{ $message }}</span> @enderror-->
-                            <textarea form="ticketing"
-                                      name="description"
+                            <textarea bind:value={config.ticketing.SubjectPlaceholder}
                                       id="description"
-                                      class="bg-gray-700/50 rounded-lg text-sm h-24 w-full mt-1 text-white/75 caret-white p-2.5 resize-none border-0 ring-1 ring-slate-900/10 focus:outline-none placeholder:font-medium placeholder:text-white/20">
-                            </textarea>
+                                      class="bg-gray-700/50 rounded-lg text-sm h-24 w-full mt-1 text-white/75 caret-white p-2.5 resize-none border-0 ring-1 ring-slate-900/10 focus:outline-none placeholder:font-medium placeholder:text-white/20"></textarea>
                         </div>
                         {#if activeTicketingTab === 2}
                             <div class="text-white/60 font-medium space-y-2">
